83ab0d8d8c53566c267d80b46681505f7628a93f
Tomasz Gielbutowicz <tomasz.gielbutowicz@gmail.com>
2011-06-06 13:22:08 +0200

    Setup GEM's

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyShowcase::Application.config.secret_token = 'ba301daac4bc73d3049fd457a6210da5f0e9f85c33f318e18b457ebbf23b5c925d362fcfab2250eb0c8c1025c3585781a91b9725f19b6ecf3b9286bdf921f729'
