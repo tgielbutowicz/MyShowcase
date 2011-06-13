efc3f22d904334a7651dbceacb77453fda1a8c75
Tomasz Gielbutowicz <tomasz.gielbutowicz@gmail.com>
2011-06-14 00:05:11 +0200

    Pages_controller bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,10 @@
 class PagesController < ApplicationController
   def home
     @title = "Home"
-    @post = Post.new if signed_in?
-    @feed_items = current_user.feed.paginate(:page => params[:page])
+    if signed_in?
+      @post = Post.new
+      @feed_items = current_user.feed.paginate(:page => params[:page])
+    end
   end
 
   def contact
