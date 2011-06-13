020760db274b034a2d81083fc6d2d9b0441180b0
Tomasz Gielbutowicz <tomasz.gielbutowicz@gmail.com>
2011-06-13 23:56:54 +0200

    Users feed temple

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_filter :authenticate, :only => [:create, :destroy]
+  before_filter :authorized_user, :only => :destroy
 
   def create
     @post  = current_user.posts.build(params[:post])
@@ -7,10 +8,21 @@ class PostsController < ApplicationController
       flash[:success] = "Post created!"
       redirect_to root_path
     else
+      @feed_items = []
       render 'pages/home'
     end
   end
 
   def destroy
+    @post.destroy
+    redirect_back_or root_path
   end
+  
+  private
+
+    def authorized_user
+      @post = Post.find(params[:id])
+      redirect_to root_path unless current_user?(@post.user)
+    end
+    
 end
